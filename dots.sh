c25becc70473f2a66edd111e856a81db6a0043dc
Kyle Connolly <wmconnolly@gmail.com>
2020-02-22 14:22:02 -0500

    using getopts to add optionality to dotfile curls

--- dots.sh
+++ dots.sh
@@ -1,22 +1,36 @@
 #!/bin/bash -i
 
+github_username="$2"/""
+github_repo+="$3"/""
+github_url="https://raw.githubusercontent.com/"
+master="master/"
+base_url="$github_url$github_username$github_repo$master"
+
+# Note: the Vim colorscheme is hardcoded
+while getopts "btv" opt; do
+	case $opt in
+		b)
+			curl -o ~/.bashrc $base_url".bashrc"
+			;;
+		t)
+			curl -o ~/.tmux.conf $base_url".tmux.conf"
+			;;
+		v)
+			curl -o ~/.vimrc $base_url".vimrc"
+			# Create dirs for custom Vim colorscheme
+			mkdir ~/.vim/
+			mkdir ~/.vim/colors/
+			curl -o ~/.vim/colors/nord.vim \
+				https://raw.githubusercontent.com/arcticicestudio/nord-vim/develop/colors/nord.vim
+			;;
+	esac
+done
+
 ### bashrc should already exist, and likely has specifics we don't want to lose.
 ### running the echo commands below to append to bashrc instead!
-### if bashrc doesn't exist, uncomment the curl to pull yours
-
+### if bashrc doesn't exist, use the `b` flag option
 
 cp ~/.bashrc ~/.bashrc.orig
-
-# curl -O https://raw.githubusercontent.com/wmconnolly/cka-dots/master/.bashrc
-curl -O https://raw.githubusercontent.com/wmconnolly/cka-dots/master/.tmux.conf
-curl -O https://raw.githubusercontent.com/wmconnolly/cka-dots/master/.vimrc
-
-# Create dirs for custom Vim colorscheme
-mkdir ~/.vim/
-mkdir ~/.vim/colors/
-# be sure to name the file the same as what's specified in your vimrc
-curl -o ~/.vim/colors/nord.vim https://raw.githubusercontent.com/arcticicestudio/nord-vim/develop/colors/nord.vim
-
 
 echo 'alias e="exit"' >>~/.bashrc
 echo 'alias c="clear"' >>~/.bashrc
