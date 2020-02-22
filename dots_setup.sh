4bb347473342d2b1d461d4b0657807b6203e930a
Kyle Connolly <wmconnolly@gmail.com>
2020-02-22 10:11:20 -0500

    echo append to bashrc, rather than replace entire file

--- dots_setup.sh
+++ dots_setup.sh
@@ -2,7 +2,20 @@
 
 curl -O https://raw.githubusercontent.com/wmconnolly/cka-dots/master/.tmux.conf
 curl -O https://raw.githubusercontent.com/wmconnolly/cka-dots/master/.vimrc
-curl -O https://raw.githubusercontent.com/wmconnolly/cka-dots/master/.bashrc
+# curl -O https://raw.githubusercontent.com/wmconnolly/cka-dots/master/.bashrc
+
+
+### bashrc should already exist, and likely has specifics we don't want to lose.
+### running the echo commands below to append to bashrc instead!
+
+echo 'alias e="exit"' >>~/.bashrc
+echo 'alias c="clear"' >>~/.bashrc
+echo 'alias ..="cd ../"' >>~/.bashrc
+echo 'alias ...="cd ../../"' >>~/.bashrc
+echo 'set -o vi' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
 
 source ~/.bashrc
 
